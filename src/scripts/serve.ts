968ec357717eca09a7b1b634b21458269dde547e
Rechie Kho <50512341+RechieKho@users.noreply.github.com>
2025-08-12 16:56:47 +0800

    Add exclusion criteria.

--- src/scripts/serve.ts
+++ src/scripts/serve.ts
@@ -6,9 +6,15 @@ import childProcess = require('child_process');
 const { projectDirectory, packageData } = metadata;
 const { execSync } = childProcess;
 const clusterDirectory = packageData['clusterDirectory'] as string;
+const excludedServices = packageData['excludedServices'] as string[];
+const excludedServicesRegExp = excludedServices.map(
+  (pattern) => new RegExp(pattern)
+);
 
 fs.readdirSync(path.join(projectDirectory, clusterDirectory)).forEach(
   (serviceName) => {
+    for (const regexp of excludedServicesRegExp)
+      if (regexp.test(serviceName)) return;
     const serviceDirectory = path.join(
       projectDirectory,
       clusterDirectory,
