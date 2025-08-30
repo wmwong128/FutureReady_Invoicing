6e7b0b482832ed850ae786dbf0b2b1a2830b6e16
wmwong128 <wm.wong128@gmail.com>
2025-08-31 01:50:23 +0800

    6

--- services/frontend/src/components/InvoiceManagement.tsx
+++ services/frontend/src/components/InvoiceManagement.tsx
@@ -5,7 +5,11 @@ import { Button } from "@/components/ui/button";
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+=======
+import { DollarSign, Download, Edit, Eye, Filter, Plus, Search, Send, Trash2, } from "lucide-react";
+>>>>>>> 9085b61ec266f4f377b1dadeec9451fc20e3bfe0
 =======
 import { DollarSign, Download, Edit, Eye, Filter, Plus, Search, Send, Trash2, } from "lucide-react";
 >>>>>>> 9085b61ec266f4f377b1dadeec9451fc20e3bfe0
@@ -34,6 +38,9 @@ import { useDeleteInvoice, useInvoiceManagement, useInvoiceStripeView, useSendIn
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> 9085b61ec266f4f377b1dadeec9451fc20e3bfe0
+=======
 >>>>>>> 9085b61ec266f4f377b1dadeec9451fc20e3bfe0
 =======
 >>>>>>> 9085b61ec266f4f377b1dadeec9451fc20e3bfe0
